e6501fabd4e234f2bea921f555d9eedd085c8764
mattsedlar <sedlarm@gmail.com>
2016-08-11 11:47:11 -0400

    Updated to work around pandas' read_excel conversion of strings to floats, updated README

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # README
 
 ## Description
-Short Python script that takes an .xlsx file and generates an html table with Bootstrap classes
+
+Short Python script that takes an .xlsx file and generates an html table with Bootstrap classes.
 
 ## Usage
 
-Place the cuddlyspork.py file in the directory of the files you wish to format. On the command line, import `cuddlyspork` then call the `cuddlyspork.table_to_html()` function. The function's parameters are the file and the table (assuming each table is on its own sheet in the Excel file) you wish to format.
+Place the cuddlyspork.py file in the directory of the files you wish to format. On the command line, import `cuddlyspork` then call the `cuddlyspork.table_to_html()` function.
 
 ```python
 
 import cuddlyspork
-cuddlyspork.table_to_html('file','Table #')
+cuddlyspork.table_to_html(file,table,ints)
+
+```
+Function parameters:
+
+**file**: *str*, the file that holds tables
+**table**: *str*, the table (`sheetname` argument in pandas' `read_excel` function) you wish to convert
+**ints**: *int,list*, a single integer or list of integers representing columns that read_excel converted from percentage to decimal that need to be converted back. Defaults to `None`
+
+Sample:
+
+```python
+
+import cuddlyspork
+cuddlyspork.table_to_html('sample.xlsx','Table 1',ints=[0,1,2])
 
 ```
 
--- cuddlyspork.py
+++ cuddlyspork.py
@@ -1,9 +1,13 @@
 def filter_html(html):
   # replace every NaN with an empty string
   html = html.replace('NaN','')
+  html = html.replace('nan%','')
 
   # switch class on table
   html = html.replace('class="dataframe"','class="table"')
+  
+  # add bg-primary class to thead
+  html = html.replace('<thead>\n    <tr ','<thead>\n    <tr class="bg-primary" ')
 
   # remove borders
   html = html.replace('border="1"','')
@@ -12,21 +16,33 @@ def filter_html(html):
   html = "<div class='table-reponsive'>" + html + "</div>"
   return html
 
+def stringify(x):
+  # coerce into float if it isn't already
+  x = float(x)
+  x = str(round(x * 100,2)) + "%"
+  return x
 
-def table_to_html(file,table):
+def transform(df,ints):
+  if type(ints) == list:
+    for int in ints:
+      df.iloc[0:,int] = df.iloc[0:,int].apply(stringify)
+      
+  else:
+    df.iloc[0:,ints] = df.iloc[0:,ints].apply(stringify)
+
+def table_to_html(file,table,ints=None):
   import pandas as pd
+  import xlrd
 
   # read the csv
-  dat = pd.ExcelFile(file)
+  dat = pd.read_excel(file,sheetname=table)
   
-  # collect data in all tabs
-  tables = {
-    sheet_name: dat.parse(sheet_name)
-      for sheet_name in dat.sheet_names
-    }
-    
+  # call transform function here if ints
+  if ints != None:
+    transform(dat,ints)
+  
   # function parameter selects the table to print
-  html = filter_html(tables[table].to_html())
+  html = filter_html(dat.to_html())
 
   # open a file and write the html to that file
   table_doc = open(table + '.html','w')
